a974456e80bb6bbfd20827c88314e6a909812bd5
Soudeh <babaeizadeh.s@gmail.com>
2024-05-01 22:12:43 +0330

    some_change

---
+++ .history/src/pages/Landing_20240501220556.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            <span> landing page</span>
+          </h1>
+          <p>
+            lorem1
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="main" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
